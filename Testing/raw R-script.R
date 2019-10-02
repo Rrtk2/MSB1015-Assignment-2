54e2e3148cdad128af152728dc229011dd065446
Rick <Rick.Reijnders@home.nl>
2019-10-02 17:28:50 +0200

    improved in-script annotation

--- Testing/raw R-script.R
+++ Testing/raw R-script.R
@@ -17,7 +17,9 @@
 #     
 #   Inputs
 #	  
-rm(list=ls())
+rm(list=ls()) # clean workspace
+
+
 #-----------------------------------------------------------------------------------------------------#
 #		Block 01		(Install &) Load packages
 #-----------------------------------------------------------------------------------------------------#
@@ -27,8 +29,8 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 
 # Insert all packages in requiredpackages
 requiredpackages <-
-  c("WikidataQueryServiceR","ggplot2","rJava","rcdk","pls","e1071","guassprLinearl","randomForest","gplots","limma","ggfortify","curl",
-	"caret","tidyverse","crayon")
+  c("WikidataQueryServiceR","ggplot2","rJava","rcdk","pls","e1071","guassprLinearl","randomForest",
+  "gplots","limma","ggfortify","curl","caret","tidyverse","crayon")
 	
 for (i in requiredpackages) {
 	if (!requireNamespace(i, quietly = TRUE))
@@ -37,21 +39,12 @@ for (i in requiredpackages) {
 	print(i)
 }
 
-# Set message styles
-Confirm.style <- green 
-Warning.style <- yellow 
-Error.style <- red
-
-
-cat(Warning.style("Done running block 01\n"))
-cat(Confirm.style("Starting...\n"))
 
 #-----------------------------------------------------------------------------------------------------#
 #		Block 02		SETTINGS
 #-----------------------------------------------------------------------------------------------------#
 # General settings
 options(stringsAsFactors 	= F)
-Verbose 					= 3		#0-3:	0= no feedback  1= prints results  2=prints results + feedback  3= prints all
 
 # Select 80% for train, 20% for test
 Randomfactor = 0.8
@@ -59,12 +52,10 @@ Randomfactor = 0.8
 # Setting seed to keep consistent results 
 set.seed(1)
 
-if(Verbose>=3) cat(Warning.style("Done running block 02a\n"))
 
 #-----------------------------------------------------------------------------------------------------#
 #		Block 03		FUNCTIONS
 #-----------------------------------------------------------------------------------------------------#
-
 # Root Mean Squared Error
 RMSE = function(yact, ypred){
   sqrt(mean((yact - ypred)^2))
@@ -94,6 +85,7 @@ query = 'SELECT DISTINCT ?comp ?compLabel ?bp ?bpUnitLabel ?CC WHERE {
 
 dataObj = query_wikidata(query)
 
+
 #-----------------------------------------------------------------------------------------------------#
 #		Block 03		bp check ; to kelvin
 #-----------------------------------------------------------------------------------------------------#
@@ -101,16 +93,16 @@ dataObj = query_wikidata(query)
 # 0°C + 273.15 = 273,15K
 idC = which(dataObj$bpUnitLabel=="degree Celsius")
 for( i in 1:length(idC)){
-dataObj[idC[i],]$bp = (dataObj[idC[i],]$bp + 273.15)
-dataObj[idC[i],]$bpUnitLabel = "kelvin"
+	dataObj[idC[i],]$bp = (dataObj[idC[i],]$bp + 273.15)
+	dataObj[idC[i],]$bpUnitLabel = "kelvin"
 }
 
 #F to Kelvin:
 # (0°F − 32) × 5/9 + 273.15 = 255,372K
 idF = which(dataObj$bpUnitLabel=="degree Fahrenheit")
 for( i in 1:length(idF)){
-dataObj[idF[i],]$bp= (dataObj[idF[i],]$bp - 32) * (5/9) + 273.15
-dataObj[idF[i],]$bpUnitLabel = "kelvin"
+	dataObj[idF[i],]$bp= (dataObj[idF[i],]$bp - 32) * (5/9) + 273.15
+	dataObj[idF[i],]$bpUnitLabel = "kelvin"
 }
 
 #additional failsafe if other metrics than Celsius and Fahrenheit are added (other metrics are removed)
@@ -122,11 +114,13 @@ dataObjBACKUP = dataObj
 # Finalize data structure
 dataObj = data.frame(dataObjBACKUP$compLabel, dataObjBACKUP$bp, dataObjBACKUP$CC)
 names(dataObj) = c("Comp","bp","CC")
+
+
 #-----------------------------------------------------------------------------------------------------#
 #							TEST FILTER TO GET LINEAR ALKANES
 #-----------------------------------------------------------------------------------------------------#
-
 dataObj = dataObj[-grep(pattern = "\\(",x = dataObj$CC),]
+
 
 #-----------------------------------------------------------------------------------------------------#
 #		Block 03		OUTLIER TESTING
@@ -137,8 +131,9 @@ dataObj$bp[dataObj$Comp=="hexatriacontane"] = 770.15
 # Dooctacontane 958.05 K (684.9 c); IN WIKIDATA AS 881.85 k
 dataObj$bp[dataObj$Comp=="Dooctacontane"] = 958.05
 
+
 #-----------------------------------------------------------------------------------------------------#
-#		Block 03		DATA VISZ
+#		Block 03		DATA VIZ
 #-----------------------------------------------------------------------------------------------------#
 # Get a general idea of how the data looks; disregarding branch effects; amount of C in compound linked to BP
 CClength_crude = nchar(gsub(pattern = "\\)",replacement = "",x = gsub(pattern = "\\(",replacement = "",x = dataObj$CC)))
@@ -149,13 +144,9 @@ BPplot = dataObj$bp[order(CClength_crude)]
 plot(plotCClength,BPplot)
 
 
-
-
-
 #-----------------------------------------------------------------------------------------------------#
 # 		Block 03		rcdk data extraction see:https://cran.r-project.org/web/packages/rcdk/vignettes/molform.html
 #-----------------------------------------------------------------------------------------------------#
-
 sp <- get.smiles.parser()
 dc <- get.desc.categories()
 
@@ -211,22 +202,19 @@ descs <- descs[, !apply(descs, 2, function(x) any(is.na(x)) )]
 descs <- descs[, !apply( descs, 2, function(x) length(unique(x)) == 1 )]
 
 if(T){
-# Correlate the descriptors with the boiling point; if these are linked, they should add some info
-corMatrix = cor(descs , dataObj$bp)
-corMatrix = as.data.frame(corMatrix[order(abs(corMatrix),decreasing = T),])
+	# Correlate the descriptors with the boiling point; if these are linked, they should add some info
+	corMatrix = cor(descs , dataObj$bp)
+	corMatrix = as.data.frame(corMatrix[order(abs(corMatrix),decreasing = T),])
 
-# select first 15 components:
-componentNames = rownames(corMatrix)[1:15]
-descs = descs[,match(colnames(descs), x = componentNames)]
+	# select first 15 components:
+	componentNames = rownames(corMatrix)[1:15]
+	descs = descs[,match(colnames(descs), x = componentNames)]
 }
 
-
-# Old method, keepin this inside for further reference
+# - BLOCKED - Old method, keepin this inside for further reference 
 if(F){
 	mydata = mydataBACKUP
-
 	descs = mydata[,-1]
-
 
 	# crude way to extract 'important' features based on correlation
 	descs <- descs[, !apply(descs, 2, function(x) any(is.na(x)) )]
@@ -235,6 +223,7 @@ if(F){
 	r2 <- r2[ r2[,1] > r2[,2] , ]
 	descs <- descs[, -unique(r2[,2])]
 }
+# - BLOCKED -
 
 # should contain nAtomLAC; the amount of c atoms
 if(!exists("descs$nAtomLAC")){
@@ -244,174 +233,165 @@ if(!exists("descs$nAtomLAC")){
 	}else{
 	names(descs)[names(descs)=="descs$nAtomLAC"] = "n"
 }
-descs$n2 = descs$n^2
 
-# add parameters from paper!
-
-#descs$w = 1/6*(descs$n-1)*(descs$n)*(descs$n+1)
-#descs$p = descs$n - 3
-# the folowing screw up the analysis:
-#descs$A = 98/descs$n^2 * descs$w
-#descs$B = 5.5*descs$p
-
-# store resulting input file in my_data; as it is used everywhere
+# Store resulting input file in my_data; as it is used everywhere
 my_data = descs
 
-#-----------------------------------------------------------------------------
-#Finish prepare data
-#-----------------------------------------------------------------------------
+# Finish prepare data
 # Add and define the 'to be predicted' column
 n_col <- ncol(my_data)
 BoilPoint = n_col+1
 my_data[,BoilPoint] = dataObj$bp
 names(my_data)[BoilPoint] = 'BoilPoint'
 
+# Ordered data is required later
 my_data = my_data[order(my_data$BoilPoint),]
 
 
 #-----------------------------------------------------------------------------------------------------#
-#							MACHINE LEARINGN
+#							MACHINE LEARNING
 #-----------------------------------------------------------------------------------------------------#
-
 #-----------------------------------------------------------------------------------------------------#
 # 				Data subsetting; Train; Test
 #-----------------------------------------------------------------------------------------------------#
-# ceiling(dim(my_data)[1]0.8)
-#make data objects
+# Define variables 
 sample.length = length(my_data[,1])
-sample.biasprob = 1 - 1:sample.length /max(sample.length )/2
 
-#make data objects
+# Higher probability to select lower (underrepresented) samples; 
+# improves range of model, increases fit and prediction power.
+# Ranges from 1 (select) to 0.5 (chance)
+sample.biasprob = 1 - 1:sample.length /max(sample.length )/2 
+
+# Make data objects
 samples.upper = sample(sample.length , floor(length(my_data[,1])*Randomfactor),prob = sample.biasprob ) #get all unique samples (not frequecies) and sample 80%
 plot(samples.upper[order(samples.upper)])
 samples.total = (1:length(my_data[,1])) # all unique samples (not frequecies)
 samples.lowerl = samples.total[!samples.total %in% samples.upper]  #which samples are sampled
-data.train = my_data[samples.upper,]#contains all upper logical
+
+# Subset data
+data.train = my_data[samples.upper,]#contains all upper 
 data.train = as.data.frame(data.train)
-
-#data.train = t(data.train)
-data.test = my_data[samples.lowerl,]#contains all lower logical
+data.test = my_data[samples.lowerl,]#contains all lower 
 data.test = as.data.frame(data.test)
-#data.test = t(data.test)
 
-# NOTE: annotations are within location [,32007]
-data.train[,BoilPoint] = data.train[,BoilPoint]
-data.test[,BoilPoint] = data.test[,BoilPoint]
-
-#remove nas
+# Remove nas
 data.train=data.train[!is.na(data.train[,1]),]
 data.test=data.test[!is.na(data.test[,1]),]
-set.seed(1)
 
-
+# Create standardized object
 xNN = data.train[,-(BoilPoint)]
 yNN = data.train[,BoilPoint]
-
 dat = data.frame(xNN, y = yNN)
 
-
-
+# Remove samples with in datasets n < 1
 data.train = dat[dat$n>0,]
 yactual.train = data.train$y
-
 data.test = data.test[data.test$n>0,]
 yactual.test = data.test$BoilPoint
 
 
-
 #-----------------------------------------------------------------------------------------------------#
-#							CARET TEST : RF
+#							CARET : pls
 #-----------------------------------------------------------------------------------------------------#
-# this block uses the caret package, likely this block will be copied and used to utelize multiple MLmethods
-
-
-# define training control
+# Define training control method; 10 - k - cross validation
 train_control <- trainControl(method="cv", number=10)
-# fix the parameters of the algorithm
-# grid <- expand.grid(.fL=c(0), .usekernel=c(FALSE))
-# train the model
-model <- train(y~., data=data.train, trControl=train_control, method="rf")
 
-ypredCARET.rf.test <- model %>% predict(data.test)
-
-# Root mean squared error
-RMSE(yactual.test, ypredCARET.rf.test)
-#	Results in: 36.76
-
-# Mean absolute error
-MAE(yactual.test, ypredCARET.rf.test)
-#	Results in: 22.11
-
-plot(yactual.test, ypredCARET.rf.test,
-     xlab="Observed BP test set", ylab="Predicted BP test set",
-     pch=19, xlim=c(0, ceiling(max(yNN)*1.1)), ylim=c(0, ceiling(max(yNN)*1.1)))
-abline(0,1, col='red')
-
-
-ypredCARET.rf.train <- model %>% predict(data.train)
-
-# Root mean squared error
-RMSE(yactual.train, ypredCARET.rf.train)
-#	Results in: 36.76
-
-# Mean absolute error
-MAE(yactual.train, ypredCARET.rf.train)
-#	Results in: 22.11
-
-plot(yactual.train, ypredCARET.rf.train,
-     xlab="Observed BP train set", ylab="Predicted BP train set",
-     pch=19, xlim=c(0, ceiling(max(yNN)*1.1)), ylim=c(0, ceiling(max(yNN)*1.1)))
-abline(0,1, col='red')
-
-#varImp(model)
-
-
-#-----------------------------------------------------------------------------------------------------#
-#							CARET TEST : pls
-#-----------------------------------------------------------------------------------------------------#
-# this block uses the caret package, likely this block will be copied and used to utelize multiple MLmethods
-
-
-# define training control
-train_control <- trainControl(method="cv", number=10)
-# fix the parameters of the algorithm
-# grid <- expand.grid(.fL=c(0), .usekernel=c(FALSE))
-# train the model
+# Train the model
 model <- train(y~., data=data.train, trControl=train_control, method="pls")
 
+# Find out what model is best
+print(model)
+
+# Find out most important variables
+plot(varImp(model))
+
+# Predict test set
 ypredCARET.pls.test <- model %>% predict(data.test)
 
 # Root mean squared error
 RMSE(yactual.test, ypredCARET.pls.test)
-#	Results in: 36.76
+# Results in: 10.61
 
 # Mean absolute error
 MAE(yactual.test, ypredCARET.pls.test)
-#	Results in: 22.11
+# Results in: 10.00
 
+# Plot ypred vs yactual of test data
 plot(yactual.test, ypredCARET.pls.test,
      xlab="Observed BP test set", ylab="Predicted BP test set",
      pch=19, xlim=c(0, ceiling(max(yNN)*1.1)), ylim=c(0, ceiling(max(yNN)*1.1)))
 abline(0,1, col='red')
 
-
+# Predict training data; check overfitting
 ypredCARET.pls.train <- model %>% predict(data.train)
 
 # Root mean squared error
 RMSE(yactual.train, ypredCARET.pls.train)
-#	Results in: 36.76
+# Results in: 13.60
 
 # Mean absolute error
 MAE(yactual.train, ypredCARET.pls.train)
-#	Results in: 22.11
+# Results in: 10.26
 
+# Plot ypred vs yactual of training data
 plot(yactual.train, ypredCARET.pls.train,
      xlab="Observed BP train set", ylab="Predicted BP train set",
      pch=19, xlim=c(0, ceiling(max(yNN)*1.1)), ylim=c(0, ceiling(max(yNN)*1.1)))
 abline(0,1, col='red')
 
+#-----------------------------------------------------------------------------------------------------#
+#							CARET : RF
+#-----------------------------------------------------------------------------------------------------#
+# Define training control method; 10 - k - cross validation
+train_control <- trainControl(method="cv", number=10)
 
-plot(varImp(model))
+# Train the model
+model <- train(y~., data=data.train, trControl=train_control, method="rf")
+
+# Find out what model is best
+print(model)
+
+# Find out most important variables
+# plot(varImp(model))
+
+# Predict test set
+ypredCARET.rf.test <- model %>% predict(data.test)
+
+# Root mean squared error
+RMSE(yactual.test, ypredCARET.rf.test)
+# Results in: 10.61
+
+# Mean absolute error
+MAE(yactual.test, ypredCARET.rf.test)
+# Results in: 10.00
+
+# Plot ypred vs yactual of test data
+plot(yactual.test, ypredCARET.rf.test,
+     xlab="Observed BP test set", ylab="Predicted BP test set",
+     pch=19, xlim=c(0, ceiling(max(yNN)*1.1)), ylim=c(0, ceiling(max(yNN)*1.1)))
+abline(0,1, col='red')
+
+# Predict training data; check overfitting
+ypredCARET.rf.train <- model %>% predict(data.train)
+
+# Root mean squared error
+RMSE(yactual.train, ypredCARET.rf.train)
+# Results in: 13.60
+
+# Mean absolute error
+MAE(yactual.train, ypredCARET.rf.train)
+# Results in: 10.26
+
+# Plot ypred vs yactual of training data
+plot(yactual.train, ypredCARET.rf.train,
+     xlab="Observed BP train set", ylab="Predicted BP train set",
+     pch=19, xlim=c(0, ceiling(max(yNN)*1.1)), ylim=c(0, ceiling(max(yNN)*1.1)))
+abline(0,1, col='red')
+
+
+
+
+
 
 
 
